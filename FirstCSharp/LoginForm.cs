7897d8b39d362f3df2066a70a1092a4cfa8259c7
ionov-e <inv.great@gmail.com>
2023-06-09 00:24:34 +0300

    First successful work with Vetmanager API

--- FirstCSharp/LoginForm.cs
+++ FirstCSharp/LoginForm.cs
@@ -6,5 +6,14 @@ namespace FirstCSharp
         {
             InitializeComponent();
         }
+
+        private async void loginButton_Click(object sender, EventArgs e)
+        {
+            string domainName = String.IsNullOrEmpty(textBoxDomainName.Text) ? "three" : textBoxDomainName.Text;
+            string apiKey = String.IsNullOrEmpty(textBoxApiKey.Text) ? "87f81046a057ec76d9e2299113d24052" : textBoxApiKey.Text;
+            VetmanagerApiGateway vetmanagerGateway = new(new HttpClient(), domainName, apiKey, false);
+            string response = await vetmanagerGateway.GetAllClientsJson();
+            MessageBox.Show(response);
+        }
     }
 }
---
+++ FirstCSharp/VetmanagerApiGateway.cs
@@ -0,0 +1,62 @@
+﻿using System.Net.Http.Headers;
+
+namespace FirstCSharp
+{
+    internal class VetmanagerApiGateway
+    {
+        private readonly HttpClient httpClient;
+        private readonly string domainName;
+        private readonly string apiKey;
+        private readonly bool isProdServer;
+
+        public VetmanagerApiGateway(HttpClient httpClient, string domainName, string apiKey, bool isProdServer)
+        {
+            this.httpClient = httpClient;
+            this.domainName = domainName;
+            this.apiKey = apiKey;
+            this.isProdServer = isProdServer;
+
+            httpClient.BaseAddress = new Uri($"https://{domainName}.test.kube-dev.vetmanager.cloud");
+            httpClient.DefaultRequestHeaders
+                  .Accept
+                  .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            httpClient.DefaultRequestHeaders.Add("X-REST-API-KEY", apiKey);
+        }
+
+        public async Task<string> GetAllClientsJson()
+        {
+            return await httpClient.GetStringAsync(
+                new PathUri(Model.client).ToString()
+                );
+        }
+
+        public enum Model
+        {
+            client,
+            pet
+        }
+
+        public class PathUri
+        {
+            private const string s_prefix = "/rest/api/";
+            private readonly Model model;
+            private readonly int id;
+
+            public PathUri(Model model)
+            {
+                this.model = model;
+            }
+
+            public PathUri(Model model, int id)
+            {
+                this.model = model;
+                this.id = id;
+            }
+
+            public override string ToString() { return s_prefix + model.ToString() + GetEndingWithIdIfPresent(); }
+
+            private string GetEndingWithIdIfPresent() { return (id == 0) ? "" : $"/{id}"; }
+        }
+    }
+}
+
