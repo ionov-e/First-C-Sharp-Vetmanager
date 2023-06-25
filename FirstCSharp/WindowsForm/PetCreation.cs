43f5f6238f8ba80daca49061b45569ad701b2996
ionov-e <inv.great@gmail.com>
2023-06-26 00:26:06 +0300

    Pet Creation Form: Visual Studio commit error fix

--- FirstCSharp/WindowsForm/PetCreation.cs
+++ FirstCSharp/WindowsForm/PetCreation.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FirstCSharp.WindowsForm
 {
-    public partial class Form1 : Form
+    public partial class PetCreation : Form
     {
-        public Form1()
+        public PetCreation()
         {
             InitializeComponent();
         }
