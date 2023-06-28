ada4034bb774e3ee3a1863637fe3deb9ec001261
ionov <ioncurly@gmail.com>
2023-06-28 18:51:41 +0300

    Login Form: added try catch for wrong credentials error

--- FirstCSharp/WindowsForm/LoginForm.cs
+++ FirstCSharp/WindowsForm/LoginForm.cs
@@ -1,4 +1,5 @@
 using FirstCSharp.DTO.RootDataWithModel.Model;
+using System.Windows.Forms;
 
 namespace FirstCSharp.WindowsForm
 {
@@ -13,10 +14,22 @@ namespace FirstCSharp.WindowsForm
         {
             string domainName = String.IsNullOrEmpty(textBoxDomainName.Text) ? "three" : textBoxDomainName.Text;
             string apiKey = String.IsNullOrEmpty(textBoxApiKey.Text) ? "87f81046a057ec76d9e2299113d24052" : textBoxApiKey.Text;
-            VetmanagerApiGateway vetmanagerGateway = new(new HttpClient(), domainName, apiKey, false);
-            Client[] response = await vetmanagerGateway.GetAllClients();
-            UserList form = new(vetmanagerGateway, response);
-            form.Show();
+
+            try { 
+                VetmanagerApiGateway vetmanagerGateway = new(new HttpClient(), domainName, apiKey, false);
+                CreatePetListForm(vetmanagerGateway);
+            } catch (Exception) { MessageBox.Show("Wasn't able to connect with provided domain ank key"); }
+        }
+
+        private async void CreatePetListForm(VetmanagerApiGateway vetmanagerApiGateway)
+        {
+            try
+            {
+                Client[] response = await vetmanagerApiGateway.GetAllClients();
+                UserList form = new(vetmanagerApiGateway, response);
+                form.Show();
+            }
+            catch (Exception ex) { MessageBox.Show("Unexpected error has happened: " + ex.Message); }
         }
     }
 }
